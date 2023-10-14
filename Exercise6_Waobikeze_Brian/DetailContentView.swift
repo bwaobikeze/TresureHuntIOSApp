b358a1add21bf4944d6cf460a30da3b2b6875e7d
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-10-14 12:58:07 -0500

    first commit

---
+++ Exercise6_Waobikeze_Brian/DetailContentView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailContentView.swift
+//  Exercise6_Waobikeze_Brian
+//
+//  Created by brian waobikeze on 10/14/23.
+//
+
+import SwiftUI
+
+struct DetailContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailContentView()
+}
---
+++ Exercise6_Waobikeze_Brian/TreasureHuntModel.swift
@@ -0,0 +1,8 @@
+//
+//  TreasureHuntModel.swift
+//  Exercise6_Waobikeze_Brian
+//
+//  Created by brian waobikeze on 10/14/23.
+//
+
+import Foundation
